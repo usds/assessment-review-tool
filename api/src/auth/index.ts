441fc49d6e6d83278c41ec40210bf77ba5dac23c
Kelvin Luu <51680299+0kl-usds@users.noreply.github.com>
2024-12-06 12:28:01 -0800

    Add error messages

--- api/src/auth/index.ts
+++ api/src/auth/index.ts
@@ -48,8 +48,10 @@ export default async function authSetup() {
   try {
     const oidcLoa1Strat = await generateStrategy();
     passport.use('oidc-loa-1', oidcLoa1Strat);
+
   } catch (err) {
-    logger.error('Error setting up login.gov');
+    logger.error('Error setting up login.gov' );
+    logger.error(JSON.stringify(err));
   }
   passport.use('token-login', demoStrategy);
 }
